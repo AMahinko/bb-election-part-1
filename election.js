233b4a0191ff15a2700291e36e8f039da93a2d3f
Alex <alex.mahinko@gmail.com>
2017-05-05 16:22:09 -0400

    root now displays candidate info

--- election.js
+++ election.js
@@ -1,5 +1,21 @@
-$(document).ready(function() {
+$(document).ready(
 
-  // Imagination!
+  $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: 'get',
+    data: {},
+    datatype: 'json'
+  }).done(function(responseData){
+    candidates = responseData['candidates']
+    arrayLength = responseData["candidates"].length;
+    counter = 0;
+    while (counter < arrayLength) {
+      current_candidate = candidates[counter];
+      $('#tracker').append('<li>' + current_candidate['name'] + '</li>');
+      $('#tracker').append('<li>' + current_candidate['votes'] + '</li>');
+      $('#tracker').append('--------------------------------------');
+      counter++;
+    }
+  })
 
-});
+);
